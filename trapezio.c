552b1863ac9c43ae75fc37334af45f693772c4e5
Ítalo Vieira <italogv@hotmail.com>
2021-02-24 02:14:32 -0300

    Fix bugs

--- trapezio.c
+++ trapezio.c
@@ -3,30 +3,29 @@
 #include <pthread.h>
 #include <math.h>
 
-#define NTHREADS 6
-
 typedef struct {
+  int t;
   int n;
   double a;
   double b;
-} Trapezoids;
+} Values;
 
 
-int f1(double x) {
+double f1(double x) {
   return 5;
 }
 
-int f2(double x) {
+double f2(double x) {
   return sin(2 * x) + cos(5 * x);
 }
 
-void * area(Trapezoids *trapezoids) {
-  int local_n = trapezoids->n / NTHREADS;
-  int local_a = trapezoids->a;
-  double h = (double) (trapezoids->b - trapezoids->a) / trapezoids->n;
-  int local_b = local_a + h;
+void * area(Values *values) {
+  int local_n = values->n / values->t;
+  double h = (values->b - values->a) / values->n;
+  double local_a = values->a + h * local_n;
+  double local_b = local_a + local_n * h;
 
-  double acc = (double) (f2(local_a) + f2(local_b)) / 2;
+  double acc = (f2(local_a) + f2(local_b)) / 2;
   for (int i = 1; i < local_n; i++) {
     float x_i = local_a + i * h;
     acc += f2(x_i);
@@ -50,23 +49,25 @@ int main(int argc, char **argv) {
   pthread_t threads[t];
   double *acc_areas[t];
 
-  Trapezoids *trapezoids = malloc(sizeof (Trapezoids));
-  trapezoids->n = n;
-  trapezoids->a = 0;
-  trapezoids->b = 2 * M_PI;
+  Values *values = malloc(sizeof (Values));
+  values->n = n;
+  values->a = 0;
+  values->b = 2 * M_PI;
+  values->t = t;
 
   // Criando threads
-  for (int i = 0; i < NTHREADS; i++) {
-    int status = pthread_create(&threads[i], NULL, (void *) area, (void *) trapezoids);
+  for (int i = 0; i < t; i++) {
+    int status = pthread_create(&threads[i], NULL, (void *) area, (void *) values);
 
     if (status < 0) {
       return 1;
     }
   }
 
+
   // Obtendo o valor de retorno das threads e somando para obter a área total
   double total_area = 0;
-  for (int i = 0; i < NTHREADS; i++) {
+  for (int i = 0; i < t; i++) {
     pthread_join(threads[i], (void *) &acc_areas[i]);
     total_area += *acc_areas[i];
   }
