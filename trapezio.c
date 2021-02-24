19e2067683e5e237a1d2c7f4734440f6cfd6d46e
Ítalo Vieira <italogv@hotmail.com>
2021-02-24 03:02:19 -0300

    Serialize using function pointer for f1 and f2

--- trapezio.c
+++ trapezio.c
@@ -8,6 +8,7 @@ typedef struct {
   int n;
   double a;
   double b;
+  double (*f) (double);
 } Values;
 
 
@@ -19,24 +20,47 @@ double f2(double x) {
   return sin(2 * x) + cos(5 * x);
 }
 
-void * area(Values *values) {
+void * subarea(Values *values) {
+  double (*f) (double) = values->f;
   int local_n = values->n / values->t;
   double h = (values->b - values->a) / values->n;
   double local_a = values->a + h * local_n;
   double local_b = local_a + local_n * h;
 
-  double acc = (f2(local_a) + f2(local_b)) / 2;
+  double acc = ((*f)(local_a) + (*f)(local_b)) / 2;
   for (int i = 1; i < local_n; i++) {
     float x_i = local_a + i * h;
-    acc += f2(x_i);
+    acc += (*f)(x_i);
   }
 
   double *area = malloc(sizeof (double));
   *area = h * acc;
 
-  printf("Área parcial é %f\n", *area);
-
   pthread_exit(area);
+}
+
+double area(Values *values) {
+  int t = values->t;
+  pthread_t threads[t];
+  double *acc_areas[t];
+
+  // Criando threads
+  for (int i = 0; i < t; i++) {
+    int status = pthread_create(&threads[i], NULL, (void *) subarea, (void *) values);
+
+    if (status < 0) {
+      exit(1);
+    }
+  }
+
+  // Obtendo o valor de retorno das threads e somando para obter a área total
+  double total_area = 0;
+  for (int i = 0; i < t; i++) {
+    pthread_join(threads[i], (void *) &acc_areas[i]);
+    total_area += *acc_areas[i];
+  }
+
+  return total_area;
 }
 
 int main(int argc, char **argv) {
@@ -46,31 +70,22 @@ int main(int argc, char **argv) {
 
   int t = strtol(argv[1], NULL, 10);
   int n = strtol(argv[2], NULL, 10);
-  pthread_t threads[t];
-  double *acc_areas[t];
 
-  Values *values = malloc(sizeof (Values));
-  values->n = n;
-  values->a = 0;
-  values->b = 2 * M_PI;
-  values->t = t;
+  printf("Cálculo será feito usando %d threads e %d trapézios\n\n", t, n);
 
-  // Criando threads
-  for (int i = 0; i < t; i++) {
-    int status = pthread_create(&threads[i], NULL, (void *) area, (void *) values);
+  Values *values_f1 = malloc(sizeof (Values));
+  values_f1->n = n;
+  values_f1->a = 0;
+  values_f1->b = 10;
+  values_f1->t = t;
+  values_f1->f = &f1;
+  printf("Teste com f1 (a = 0, b = 10): %.2e\n", area(values_f1));
 
-    if (status < 0) {
-      return 1;
-    }
-  }
-
-
-  // Obtendo o valor de retorno das threads e somando para obter a área total
-  double total_area = 0;
-  for (int i = 0; i < t; i++) {
-    pthread_join(threads[i], (void *) &acc_areas[i]);
-    total_area += *acc_areas[i];
-  }
-
-  printf("%.2e\n", total_area);
+  Values *values_f2 = malloc(sizeof (Values));
+  values_f2->n = n;
+  values_f2->a = 0;
+  values_f2->b = 2 * M_PI;
+  values_f2->t = t;
+  values_f2->f = &f2;
+  printf("Teste com f2 (a = 0, b = 2*PI): %.2e\n", area(values_f2));
 }
