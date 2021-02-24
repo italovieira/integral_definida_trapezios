64f5ca1e4ad12e59a2c11bf94f8403a173fcef07
Ítalo Vieira <italogv@hotmail.com>
2021-02-24 00:47:17 -0300

    Update

--- trapezio.c
+++ trapezio.c
@@ -1,46 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <math.h>
 
-#define NTHREADS 5
+#define NTHREADS 6
 
 typedef struct {
   int n;
-  float a;
-  float b;
+  double a;
+  double b;
 } Trapezoids;
 
 
-int f(int x) {
+int f1(double x) {
   return 5;
+}
+
+int f2(double x) {
+  return sin(2 * x) + cos(5 * x);
 }
 
 void * area(Trapezoids *trapezoids) {
   int local_n = trapezoids->n / NTHREADS;
   int local_a = trapezoids->a;
-  float h = (float) (trapezoids->b - trapezoids->a) / trapezoids->n;
+  double h = (double) (trapezoids->b - trapezoids->a) / trapezoids->n;
   int local_b = local_a + h;
 
-  float acc = (float) (f(local_a) + f(local_b)) / 2;
+  double acc = (double) (f2(local_a) + f2(local_b)) / 2;
   for (int i = 1; i < local_n; i++) {
     float x_i = local_a + i * h;
-    acc += f(x_i);
+    acc += f2(x_i);
   }
 
-  float *area = malloc(sizeof (float));
+  double *area = malloc(sizeof (double));
   *area = h * acc;
+
+  printf("Área parcial é %f\n", *area);
 
   pthread_exit(area);
 }
 
-int main(void) {
-  pthread_t threads[NTHREADS];
-  float *acc_areas[NTHREADS];
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    return 1;
+  }
+
+  int t = strtol(argv[1], NULL, 10);
+  int n = strtol(argv[2], NULL, 10);
+  pthread_t threads[t];
+  double *acc_areas[t];
 
   Trapezoids *trapezoids = malloc(sizeof (Trapezoids));
-  trapezoids->n = 200;
+  trapezoids->n = n;
   trapezoids->a = 0;
-  trapezoids->b = 10;
+  trapezoids->b = 2 * M_PI;
 
   // Criando threads
   for (int i = 0; i < NTHREADS; i++) {
@@ -52,7 +65,7 @@ int main(void) {
   }
 
   // Obtendo o valor de retorno das threads e somando para obter a área total
-  float total_area = 0;
+  double total_area = 0;
   for (int i = 0; i < NTHREADS; i++) {
     pthread_join(threads[i], (void *) &acc_areas[i]);
     total_area += *acc_areas[i];
